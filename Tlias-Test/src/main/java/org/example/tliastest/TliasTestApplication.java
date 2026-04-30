4cae52db1fa308e29033e6db49bcdf4f81eb81da
TeddiZen <2576903455@qq.com>
2026-04-30 22:58:51 +0800

    搭建一个新的项目练习增删改查

---
+++ Tlias-Test/src/main/java/org/example/tliastest/TliasTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.tliastest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TliasTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TliasTestApplication.class, args);
+    }
+
+}
