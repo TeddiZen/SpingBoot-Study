16d0ca8473e01fd1cb811db236fc31337f99583f
TeddiZen <2576903455@qq.com>
2026-05-01 00:05:54 +0800

    getMapping

--- Tlias-Test/src/main/java/org/example/tliastest/controller/DeptController.java
+++ Tlias-Test/src/main/java/org/example/tliastest/controller/DeptController.java
@@ -6,6 +6,7 @@ import org.example.tliastest.pojo.Result;
 import org.example.tliastest.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,7 @@ public class DeptController {
     @Autowired
     private DeptService deptService;
 
-    @RequestMapping(value = "/depts", method = RequestMethod.GET)
+    @GetMapping("/depts")
     public Result list() {
         System.out.println("查询所有部门数据");
         List<Dept> depts = deptService.findAll();
