280d9e3e2fd3851bac557235e0043f3e5920b163
TeddiZen <2576903455@qq.com>
2026-05-12 18:17:43 +0800

    建立SQL的XML框架

--- Tlias-Test/src/main/java/org/example/tliastest/mapper/EmpMapper.java
+++ Tlias-Test/src/main/java/org/example/tliastest/mapper/EmpMapper.java
@@ -3,6 +3,7 @@ package org.example.tliastest.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.example.tliastest.pojo.Emp;
+import org.example.tliastest.pojo.EmpSelect;
 
 import java.util.List;
 
@@ -15,4 +16,6 @@ public interface EmpMapper {
 
     @Select("SELECT COUNT(*) FROM emp e LEFT JOIN dept d ON e.dept_id = d.id")
     Integer selectCount();
+
+    EmpSelect selectEmp();
 }
---
+++ Tlias-Test/src/main/java/org/example/tliastest/pojo/EmpSelect.java
@@ -0,0 +1,5 @@
+package org.example.tliastest.pojo;
+
+public class EmpSelect {
+
+}
