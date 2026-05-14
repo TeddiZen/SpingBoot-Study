776dff5c8f563c34a54d8a63f9f0d3db46701a27
TeddiZen <2576903455@qq.com>
2026-05-14 23:34:36 +0800

    完善文件上传接口

---
+++ Tlias-Test/src/main/java/org/example/tliastest/controller/UploadController.java
@@ -0,0 +1,31 @@
+package org.example.tliastest.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.io.ResolverUtil;
+import org.example.tliastest.pojo.Result;
+import org.example.tliastest.utils.AliyunOSSOperator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@RestController
+public class UploadController {
+
+    @Autowired
+    AliyunOSSOperator ossOperator;
+
+    @PostMapping("/upload")
+    public Result upload(MultipartFile file) throws Exception {
+        try {
+            log.info("上传文件: {}", file.getOriginalFilename());
+            String url = ossOperator.upload(file.getBytes(), file.getOriginalFilename());
+            log.info("上传成功: {}", url);
+            return Result.success(url);
+        } catch (Exception e) {
+            return Result.error("上传失败");
+        }
+    }
+}
